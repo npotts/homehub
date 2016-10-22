08718b3809b2054245c00bff9162964e5125e6eb
npotts <npotts@users.noreply.github.com>
2016-10-22 11:48:25 -0600

    erm..  further thinking

--- brainiac/config.go
+++ brainiac/config.go
@@ -10,51 +10,62 @@ package brainiac
 
 import (
 	"github.com/alecthomas/kingpin"
+	// "os"
 )
 
-type brainiacConfig struct {
-	Driver *string
-	Source *string
-	PIDLoc *string
+/*BrainiacConfig is a configuration structure for a brianiac instance*/
+type BrainiacConfig struct {
+	Driver  string
+	Source  string
+	PIDLock string
 
 	//http backend
-	ListenHTTP   *bool
-	HTTPUser     *string
-	HTTPPassword *string
-	HTTPPort     *int
+	ListenHTTP   bool
+	HTTPUser     string
+	HTTPPassword string
+	HTTPPort     int
 
 	//Listen on a UDP socket
-	ListenUDP *bool
-	UDPPort   *int
+	ListenUDP bool
+	UDPPort   int
 
 	//ZMQ settings
-	ListenZmq *bool
-	ZmqAllow  *[]string
-	ZmqListen *string
+	ListenZmq bool
+	ZmqAllow  []string
+	ZmqListen string
 }
 
 var (
-	app = kingpin.New("brianiac", "HomeHub's Database Agent")
-	// http  = app.F
-	config = &brainiacConfig{
-		Driver: app.Flag("driver", "Specify the drive to use.  One of 'sqlite3', 'postgres' or 'mysql'").Short('d').Default("sqlite3").String(),
-		Source: app.Flag("source", `Source of the database, usually something like "file.db", ":memory:", "postgres://user:password@server/database"`).Short('s').Default("brainiac.db").String(),
-		PIDLoc: app.Flag("pid", `Path to PID file`).Short('i').Default("brianiac.pid").String(),
-
-		ListenHTTP:   app.Flag("http", `Listen for requests over HTTP`).Short('H').Default("False").Bool(),
-		HTTPUser:     app.Flag("user", `Username to require for over HTTP`).Short('l').Default("brainiac").String(),
-		HTTPPassword: app.Flag("password", `Password for login over HTTP`).Short('p').Default("brainiac").String(),
-		HTTPPort:     app.Flag("http-port", `Port to start HTTP daemon on`).Short('P').Default("8080").Int(),
-
-		ListenUDP: app.Flag("udp", `Listen for requests over UDP`).Short('u').Default("False").Bool(),
-		UDPPort:   app.Flag("udp-port", `Port to listen for incoming UDP packets on`).Short('U').Default("8080").Int(),
-
-		ListenZmq: app.Flag("zmq", `Listen for requests over ZMQ`).Short('z').Default("False").Bool(),
-		ZmqAllow:  app.Flag("zmq-allow", `Allow ZMQ access from these hosts alone`).Short('a').Default("").Strings(),
-		ZmqListen: app.Flag("zmq-listen", `Port to listen for incoming ZMQ clients`).Short('Z').Default("tcp://*:8081").String(),
-	}
+	app          = kingpin.New("brianiac", "HomeHub's Database Agent")
+	driver       = app.Flag("driver", "Specify the drive to use.  One of 'sqlite3', 'postgres' or 'mysql'").Short('d').Default("sqlite3").String()
+	source       = app.Flag("source", `Source of the database, usually something like "file.db", ":memory:", "postgres://user:password@server/database"`).Short('s').Default("brainiac.db").String()
+	pidlock      = app.Flag("pid", `Path to PID file`).Short('i').Default("brianiac.pid").String()
+	listenHTTP   = app.Flag("http", `Listen for requests over HTTP`).Short('H').Default("False").Bool()
+	httpUser     = app.Flag("user", `Username to require for over HTTP`).Short('l').Default("brainiac").String()
+	httpPassword = app.Flag("password", `Password for login over HTTP`).Short('p').Default("brainiac").String()
+	httpPort     = app.Flag("http-port", `Port to start HTTP daemon on`).Short('P').Default("8080").Int()
+	listenUDP    = app.Flag("udp", `Listen for requests over UDP`).Short('u').Default("False").Bool()
+	udpPort      = app.Flag("udp-port", `Port to listen for incoming UDP packets on`).Short('U').Default("8080").Int()
+	listenZmq    = app.Flag("zmq", `Listen for requests over ZMQ`).Short('z').Default("False").Bool()
+	zmqAllow     = app.Flag("zmq-allow", `Allow ZMQ access from these hosts alone`).Short('a').Default("").Strings()
+	zmqListen    = app.Flag("zmq-listen", `Port to listen for incoming ZMQ clients`).Short('Z').Default("tcp://*:8081").String()
 )
 
-func parse(args []string) {
+/*ConfigForArgs returns a Brainiac config for a given set of arguments.*/
+func ConfigForArgs(args []string) *BrainiacConfig {
 	kingpin.MustParse(app.Parse(args))
+	return &BrainiacConfig{
+		Driver:       *driver,
+		Source:       *source,
+		PIDLock:      *pidlock,
+		ListenHTTP:   *listenHTTP,
+		HTTPUser:     *httpUser,
+		HTTPPassword: *httpPassword,
+		HTTPPort:     *httpPort,
+		ListenUDP:    *listenUDP,
+		UDPPort:      *udpPort,
+		ListenZmq:    *listenZmq,
+		ZmqAllow:     *zmqAllow,
+		ZmqListen:    *zmqListen,
+	}
 }
