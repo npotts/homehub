23db8cd3a387d963904d7ba3d570f4ef7cce213b
npotts <npotts@users.noreply.github.com>
2016-10-23 00:05:35 -0600

    added Validity, Datam, and alphabetic variables

--- brainiac/interfaces.go
+++ brainiac/interfaces.go
@@ -8,10 +8,36 @@
 
 package brainiac
 
-import ()
+import (
+	"regexp"
+)
 
 /*function call to both register and store data*/
-type regstore *func(table string, data map[string]interface{}) error
+type regstore *func(table string, datam Datam) error
+
+//something from a-z and A-Z
+type alphabet string
+
+var realph = regexp.MustCompile("[a-zA-Z]+") //at least one char
+
+/*Valid returns true only if it contains valid characters*/
+func (a alphabet) Valid() bool {
+	return realph.MatchString(string(a))
+}
+
+/*Datam is what all insertable things should map to*/
+type Datam struct {
+	Table alphabet                 `json:"table"`
+	Data  map[alphabet]interface{} `json:"data"`
+}
+
+func (d Datam) Valid() bool {
+	ok := d.Table.Valid()
+	for label := range d.Data {
+		ok = ok && label.Valid()
+	}
+	return ok
+}
 
 /*listener listen on something and call registerFxn when*/
 type stoppable interface {
