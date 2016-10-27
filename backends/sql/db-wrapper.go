87e495c5c91996494a9dca0cd71e5908c95bdd98
npotts <npotts@users.noreply.github.com>
2016-10-26 23:47:04 -0600

    Added first sql database wrapper.  Now it needs a client application

---
+++ backends/sql/db-wrapper.go
@@ -0,0 +1,64 @@
+/*
+Copyright (c) 2016 Nick Potts
+Licensed to You under the GNU GPLv3
+See the LICENSE file at github.com/npotts/homehub/LICENSE
+
+This file is part of the HomeHub project
+*/
+
+package database
+
+import (
+	"github.com/jmoiron/sqlx"
+	_ "github.com/lib/pq"           //postgres support
+	_ "github.com/mattn/go-sqlite3" //sqlite3  support
+
+	"github.com/npotts/homehub"
+)
+
+/*SQLBackend wraps a database and functions as a homehub.Backend*/
+type SQLBackend struct {
+	dialect string
+	db      *sqlx.DB //database backend
+}
+
+/*Backend returns a backend and nil error if successful*/
+func Backend(driver, source string) (homehub.Backend, error) {
+	return New(driver, source)
+}
+
+/*New returns an intialized Brianiac or a non-nil error*/
+func New(driver, source string) (*SQLBackend, error) {
+	db, err := sqlx.Connect(driver, source)
+	if err != nil {
+		return nil, err
+	}
+	return &SQLBackend{dialect: driver, db: db}, nil
+}
+
+/*Register attempts to register the passed piece of data
+into the database - usually this means creating a table*/
+func (q *SQLBackend) Register(datam homehub.Datam) error {
+	sql, err := datam.SqlCreate(q.dialect)
+	if err != nil {
+		return err
+	}
+	_, err = q.db.Exec(sql)
+	return err
+}
+
+/*Store attempts to store the passed piece of data
+into the database*/
+func (q *SQLBackend) Store(datam homehub.Datam) error {
+	query, args, err := datam.NamedExec()
+	if err != nil {
+		return err
+	}
+	_, err = q.db.NamedExec(query, args)
+	return err
+}
+
+/*Stop shuts down the database*/
+func (q *SQLBackend) Stop() {
+	q.db.Close()
+}
---
+++ backends/sql/db-wrapper_test.go
@@ -0,0 +1,59 @@
+/*
+Copyright (c) 2016 Nick Potts
+Licensed to You under the GNU GPLv3
+See the LICENSE file at github.com/npotts/homehub/LICENSE
+
+This file is part of the HomeHub project
+*/
+
+package database
+
+import (
+	"testing"
+
+	"github.com/npotts/homehub"
+)
+
+func Test_New(t *testing.T) {
+	_, err := New("postgres", ":memory:")
+	if err == nil {
+		t.Errorf("Should error out when we do not have a correct connection string")
+	}
+
+	n, err := New("sqlite3", ":memory:")
+	defer n.Stop()
+	if err != nil {
+		t.Errorf("Should not fail with memory database: %v", err)
+	}
+
+	nn, err := Backend("sqlite3", ":memory:")
+	if err != nil {
+		t.Errorf("Should not fail with memory database: %v", err)
+	} else {
+		nn.Stop()
+	}
+}
+
+func TestSQLBackend_RegisterStore(t *testing.T) {
+	bad, good := homehub.Datam{}, homehub.GoodSample
+
+	q, e := New("sqlite3", ":memory:")
+	if e != nil {
+		t.Fatalf("Couldnt start instance: %v", e)
+	}
+
+	if e := q.Register(bad); e == nil {
+		t.Errorf("Should get an error registering garbage")
+	}
+	if e := q.Store(bad); e == nil {
+		t.Errorf("Should get an error storing garbage")
+	}
+
+	if e := q.Register(good); e != nil {
+		t.Errorf("Should get an error registering garbage")
+	}
+	if e := q.Store(good); e != nil {
+		t.Errorf("Should get an error storing garbage")
+	}
+
+}
