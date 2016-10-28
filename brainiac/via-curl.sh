c92989affb9303cc4b1e65d16ecf339f51047f91
npotts <npotts@users.noreply.github.com>
2016-10-27 22:58:40 -0600

    Use auth

--- brainiac/via-curl.sh
+++ brainiac/via-curl.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
 
-curl --digest -u brainiac:brainiac -sw "Return Code: %{http_code}"  -X "PUT" -o curl.data --data-raw '{"a":"table", "data": {"field": 1.0}}' http://localhost:8080
+curl -sw "Return Code: %{http_code}" -u brainiac:brainiac -X "PUT" -o curl.data --data-raw '{"table":"a", "data": {"field": 1.0}}' http://localhost:8080
 echo 
 
 
 
 #insert 100 lines
-for i in {1..10}; do
-	curl --digest -u brainiac:brainiac -sw "Return Code: %{http_code}" -X "POST"  -o curl.data --data-raw '{"a":"table", "data": {"field": 1.0}}' http://localhost:8080
+for i in {1..1000}; do
+	curl -sw "Return Code: %{http_code}" -X "POST"  -u brainiac:brainiac -o curl.data --data-raw '{"table":"a", "data": {"field": 1.0}}' http://localhost:8080
 	echo
 done
+
+curl -sw "Return Code: %{http_code}" -X "GET"  -u brainiac:brainiac -o curl.data --data-raw '{"table":"a", "data": {"field": 1.0}}' http://localhost:8080
+echo
+cat  curl.data
